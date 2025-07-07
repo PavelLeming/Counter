0e361f7884b05b307e02e92f25bd860744f22608
PavelLeming <pavelponomarev2002@gmail.com>
2025-07-07 20:37:18 +0700

    v1.1

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -6,11 +6,13 @@ public class Counter : MonoBehaviour
 {
     private float _delay = 0.5f;
     private int _count = 0;
+    private Coroutine _countup;
+    CounterView counterView = new CounterView();
     private bool isRun = true;
 
     private void Start()
     {
-        StartCoroutine(Countup(_delay));
+        _countup = StartCoroutine(Countup(_delay));
     }
 
     private void Update()
@@ -19,23 +21,26 @@ public class Counter : MonoBehaviour
         {
             if (isRun)
             {
-                StopAllCoroutines();
+                StopCoroutine(_countup);
                 isRun = false;
             }
             else
             {
                 StartCoroutine(Countup(_delay));
-                isRun= true;
+                isRun = true;
             }
         }
     }
 
     private IEnumerator Countup(float delay)
     {
-        while (true)
+        var wait = new WaitForSeconds(delay);
+
+        while (enabled)
         {
-            Debug.Log(_count++);
-            yield return new WaitForSeconds(delay);
+            counterView.ShowCount(_count);
+            _count++;
+            yield return wait;
         }
     }
 }
---
+++ Assets/Scripts/CounterView.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterView : MonoBehaviour
+{
+    public void ShowCount(int count)
+    {
+        Debug.Log(count);
+    }
+}
